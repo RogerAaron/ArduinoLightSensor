1c04511df89e652b324516da218a501ea707b9fb
samdoss <samdoss@live.com>
2019-07-14 21:41:57 +0530

    Arduino Project files

---
+++ ArduinoDataReader/Form1.cs
@@ -0,0 +1,83 @@
+﻿using Samdoss.CommonLayer;
+using Samdoss.DataLayer;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArduinoDataReader
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		Thread demoThread; // = new Thread(new ThreadStart(CallToChildThread));
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+			DataStore ds = new DataStore();
+			ds.ScreenMode = ScreenMode.Add;
+			while (true)
+			{
+				string data = "31";
+				if (!string.IsNullOrEmpty(data))
+				{
+					ds.DataValue = data;
+					ds.Commit();
+					Console.WriteLine(data);
+				}
+			}
+		}
+
+		public void CallToChildThread()
+		{
+			while (true)
+			{
+				if (arduinoChart.IsHandleCreated)
+				{
+
+					this.Invoke((MethodInvoker)delegate { arduinoCharts(); });
+				}
+			}						
+		}
+
+		private void arduinoCharts()
+		{
+			
+			DataStore ds = new DataStore();
+			ds.GetDataStored();
+			arduinoChart.Series["Series1"].Points.AddY(Convert.ToInt32(ds.DataValue));
+		}
+
+
+		private void btnClose_Click(object sender, EventArgs e)
+		{
+			Close();
+		}
+
+		private void btnStop_Click(object sender, EventArgs e)
+		{
+			this.demoThread.Abort();
+			btnStart.Enabled = true;
+		}
+
+		private void btnStart_Click(object sender, EventArgs e)
+		{
+			arduinoChart.Series["Series1"].Points.Clear();
+			this.demoThread = new Thread(new ThreadStart(this.CallToChildThread));
+			this.demoThread.IsBackground = true;
+			this.demoThread.Start();
+			btnStart.Enabled = false;
+		}
+	}
+}
---
+++ MainApp/WebApi.Serial/Controllers/ValuesController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Serial.Controllers
+{
+	[Route("api/[controller]")]
+	public class ValuesController : Controller
+	{ 
+		// GET api/values/5
+		[HttpGet]
+		public string Get(string id)
+		{
+			SendToDB(id);
+			return "value";
+		}
+
+		// POST api/values
+		[HttpPost]
+		public void Post([FromBody]TestData testData)
+		{
+			SendToDB(testData);
+		}
+
+		private void SendToDB(TestData dataValue)
+		{
+			if (!string.IsNullOrEmpty(dataValue.DataValue) && dataValue.DataValue != "0")
+			{
+				SqlConnection myCon = null;
+				myCon = new SqlConnection("Data Source=db204.my-hosting-panel.com;Initial Catalog=sym147_arduinodb;Connection Timeout=180;Persist Security Info=True;User ID=sym147_projectarduino;Password=arduinosam147");
+				try
+				{
+
+					myCon.Open();
+
+					SqlCommand cmd = new SqlCommand("spAddEdit", myCon);
+					cmd.CommandType = CommandType.StoredProcedure;
+					cmd.Parameters.AddWithValue("DataValue", dataValue.DataValue);
+					cmd.Parameters.AddWithValue("CurrentDateTime", DateTime.Now);
+					cmd.CommandTimeout = 300;
+					cmd.ExecuteNonQuery();
+
+					myCon.Close();
+				}
+				catch (Exception ex)
+				{
+					string exception = ex.Message;
+				}
+				finally
+				{
+					myCon.Close();
+				}
+			}
+		}
+
+		private void SendToDB(string id)
+		{
+			if (!string.IsNullOrEmpty(id) && id != "0")
+			{
+				SqlConnection myCon = null;
+				myCon = new SqlConnection("Data Source=db204.my-hosting-panel.com;Initial Catalog=sym147_arduinodb;Connection Timeout=180;Persist Security Info=True;User ID=sym147_projectarduino;Password=arduinosam147");
+				try
+				{
+					myCon.Open();
+
+					SqlCommand cmd = new SqlCommand("spAddEdit", myCon);
+					cmd.CommandType = CommandType.StoredProcedure;
+					cmd.Parameters.AddWithValue("DataValue", id);
+					cmd.Parameters.AddWithValue("CurrentDateTime", DateTime.Now);
+					cmd.CommandTimeout = 300;
+					cmd.ExecuteNonQuery();
+
+					myCon.Close();
+				}
+				catch (Exception ex)
+				{
+					string exception = ex.Message;
+				}
+				finally
+				{
+					myCon.Close();
+				}
+			}
+		}
+
+		// PUT api/values/5
+		[HttpPut("{id}")]
+		public void Put(int id, [FromBody]string value)
+		{
+		}
+
+		// DELETE api/values/5
+		[HttpDelete("{id}")]
+		public void Delete(int id)
+		{
+		}
+	}
+
+	public class TestData
+	{
+		public string DataValue { get; set; }
+		
+	}
+
+}
